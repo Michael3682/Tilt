ffe9e47ba4530e42ee9a3114204df6cdb29f65c3
Michael3682 <152974233+Michael3682@users.noreply.github.com>
2024-04-06 16:43:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 const container = document.querySelector('.container')
-const box1 = document.getElementById('box1')
-const box2 = document.getElementById('box2')
-const box3 = document.getElementById('box3')
-const box4 = document.getElementById('box4')
-const box5 = document.getElementById('box5')
-const box6 = document.getElementById('box6')
-const box7 = document.getElementById('box7')
-const box8 = document.getElementById('box8')
-const box9 = document.getElementById('box9')
+// const box1 = document.getElementById('box1')
+// const box2 = document.getElementById('box2')
+// const box3 = document.getElementById('box3')
+// const box4 = document.getElementById('box4')
+// const box5 = document.getElementById('box5')
+// const box6 = document.getElementById('box6')
+// const box7 = document.getElementById('box7')
+// const box8 = document.getElementById('box8')
+// const box9 = document.getElementById('box9')
 
-container.addEventListener('mouseleave', () => {
-    container.style.transform = 'rotateX(0deg) rotateY(0deg)'
+// container.addEventListener('mouseleave', () => {
+//     container.style.transform = 'rotateX(0deg) rotateY(0deg)'
+// })
+// box1.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(-20deg) rotateY(20deg)'
+// })
+// box2.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(-20deg) rotateY(0deg)'
+// })
+// box3.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(-20deg) rotateY(-20deg)'
+// })
+// box4.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(0deg) rotateY(20deg)'
+// })
+// box5.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(0deg) rotateY(0deg)'
+// })
+// box6.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(0deg) rotateY(-20deg)'
+// })
+// box7.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(20deg) rotateY(20deg)'
+// })
+// box8.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(20deg) rotateY(0deg)'
+// })
+// box9.addEventListener('mouseover', () => {
+//     container.style.transform = 'rotateX(20deg) rotateY(-20deg)'
+// })
+
+document.addEventListener('mousemove', (e) => {
+    rotateElement(e, container)
 })
-box1.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(-20deg) rotateY(20deg)'
-})
-box2.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(-20deg) rotateY(0deg)'
-})
-box3.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(-20deg) rotateY(-20deg)'
-})
-box4.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(0deg) rotateY(20deg)'
-})
-box5.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(0deg) rotateY(0deg)'
-})
-box6.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(0deg) rotateY(-20deg)'
-})
-box7.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(20deg) rotateY(20deg)'
-})
-box8.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(20deg) rotateY(0deg)'
-})
-box9.addEventListener('mouseover', () => {
-    container.style.transform = 'rotateX(20deg) rotateY(-20deg)'
-})
+
+function rotateElement(event, element) {
+    const x = event.clientX
+    const y = event.clientY
+    
+    const middleX = window.innerWidth / 2
+    const middleY = window.innerHeight / 2
+
+    const offsetX = ((x - middleX) / middleX) * 45
+    const offsetY = ((y - middleY) / middleY) * 45
+
+    element.style.setProperty("--rotateX", -1 * offsetY + "deg")
+    element.style.setProperty("--rotateY", offsetX + "deg")
+}
